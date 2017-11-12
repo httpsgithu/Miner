ded4a6fa19f6e9a487aaec6a482a6d11998b94a9
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-11-12 15:12:29 -0800

    Basic communication working.

---
+++ CommonMiner/Network/ServerNotAvailableException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace HD
+{
+  [Serializable]
+  internal class ServerNotAvailableException : Exception
+  {
+    public ServerNotAvailableException()
+    {
+    }
+
+    public ServerNotAvailableException(string message) : base(message)
+    {
+    }
+
+    public ServerNotAvailableException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected ServerNotAvailableException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+  }
+}
