6bb4ee55594d30d8bb88218ec7f80f38aa8a784a
se5a <jon.se5a@gmail.com>
2017-11-23 11:25:48 +1300

    eto forms implementation start.

---
+++ HardyMinerUI/Xaml/StatsBox.xeto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+
+namespace HardyMinerUI.Xaml
+{
+    public class StatsBox : Panel
+    {
+        public StatsBox()
+        {
+            XamlReader.Load(this);
+        }
+    }
+}
